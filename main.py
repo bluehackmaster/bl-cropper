0d34bea3de728c06ed3832d03f26d79906fcf4fa
Youngbok Yoon <master@bluehack.net>
2017-09-26 17:37:59 +0900

    [BL-17] Removed multiprocessing codes

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
 import uuid
 
+import os
 from google.cloud import bigquery
+
+import numpy as np
+import io
+import time
+import argparse
+import multiprocessing
+import tensorflow as tf
+from multiprocessing import Queue, Pool
+from PIL import Image
+import urllib
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+try:
+    from StringIO import StringIO
+except ImportError:
+    from io import StringIO
+
+CWD_PATH = os.getcwd()
+
+PATH_TO_CKPT = os.path.join(CWD_PATH, 'frozen_inference_graph.pb')
+PATH_TO_LABELS = os.path.join(CWD_PATH, 'label_map.pbtxt')
+
+NUM_CLASSES = 4
+
+# Loading label map
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES,
+                                                            use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
 
 def query():
     client = bigquery.Client.from_service_account_json(
         'BlueLens-d8117bd9e6b1.json')
 
-    query = 'SELECT * FROM stylelens.stylenanda LIMIT 10;'
+    detection_graph = tf.Graph()
+    with detection_graph.as_default():
+        od_graph_def = tf.GraphDef()
+        with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+            serialized_graph = fid.read()
+            od_graph_def.ParseFromString(serialized_graph)
+            tf.import_graph_def(od_graph_def, name='')
+
+        sess = tf.Session(graph=detection_graph)
+
+    query = 'SELECT host_url, image_url FROM stylelens.stylenanda LIMIT 5;'
 
     query_job = client.run_async_query(str(uuid.uuid4()), query)
 
@@ -17,7 +57,70 @@ def query():
     destination_table = query_job.destination
     destination_table.reload()
     for row in destination_table.fetch_data():
-        print(row)
+        URL = '' + row[0] + row[1]
+        f = urllib.request.urlopen(URL)
+        image = Image.open(f)
+        image_np = load_image_into_numpy_array(image)
+        out_image = detect_objects(image_np, sess, detection_graph)
+
+        img = Image.fromarray(out_image, 'RGB')
+        img.show()
+
+    sess.close()
+
+
+def load_image_into_numpy_array(image):
+  (im_width, im_height) = image.size
+  return np.array(image.getdata()).reshape(
+      (im_height, im_width, 3)).astype(np.uint8)
+
+
+def detect_objects(image_np, sess, detection_graph):
+    # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+    image_np_expanded = np.expand_dims(image_np, axis=0)
+    image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+    # Each box represents a part of the image where a particular object was detected.
+    boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+    # Each score represent how level of confidence for each of the objects.
+    # Score is shown on the result image, together with the class label.
+    scores = detection_graph.get_tensor_by_name('detection_scores:0')
+    classes = detection_graph.get_tensor_by_name('detection_classes:0')
+    num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+    # Actual detection.
+    (boxes, scores, classes, num_detections) = sess.run(
+        [boxes, scores, classes, num_detections],
+        feed_dict={image_tensor: image_np_expanded})
+
+    # Visualization of the results of a detection.
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        image_np,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8)
+    print(image_np)
+    return image_np
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-src', '--source', dest='video_source', type=int,
+                        default=0, help='Device index of the camera.')
+    parser.add_argument('-wd', '--width', dest='width', type=int,
+                        default=480, help='Width of the frames in the video stream.')
+    parser.add_argument('-ht', '--height', dest='height', type=int,
+                        default=360, help='Height of the frames in the video stream.')
+    parser.add_argument('-num-w', '--num-workers', dest='num_workers', type=int,
+                        default=2, help='Number of workers.')
+    parser.add_argument('-q-size', '--queue-size', dest='queue_size', type=int,
+                        default=5, help='Size of the queue.')
+    args = parser.parse_args()
+
+    logger = multiprocessing.log_to_stderr()
+    logger.setLevel(multiprocessing.SUBDEBUG)
+
     query()
